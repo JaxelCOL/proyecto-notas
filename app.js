89d6520c01f2a92b0cdbf1ba446f348160d810b5
Axel Cortes <juliancortes5972@gmail.com>
2026-04-12 01:17:11 -0500

    Se corrige estructura

--- app.js
+++ app.js
@@ -1,60 +1,56 @@
 function menu() 
-let opcion = "" ; 
+    let opcion = "" ; 
 
-while (opcion !== "0")
-opcion = prompt(
-    "1. ver todos/n" +
-    "2. aprobado/n" +
-    "3. reprobado/n" +
-    "4. promedio/n" +
-    "5. ordenar/n" +
-    "6. mayusculas/n" +
-    "7. clasificar notas/n" +
-    "0. salir"
+    while (opcion !== "0"){
+        opcion = prompt(
 
-);
+            "1. ver todos/n" +
+            "2. aprobado/n" +
+            "3. reprobado/n" +
+            "4. promedio/n" +
+            "5. ordenar/n" +
+            "6. mayusculas/n" +
+            "7. clasificar notas/n" +
+            "0. salir"
 
-switch (opcion) {
-    case "1"
-    mostrarAprendices(aprendices);
-    break;
+        );
 
-    case "2"
-    console.log(aprobados(aprendices));
-    break; 
+        switch (opcion) {
+            case "1"
+                mostrarAprendices(aprendices);
+                break;
 
-    case "3"
-    console.log(reprobado(aprendices));
-    break;
+            case "2"
+                console.log(aprobados(aprendices));
+                break; 
 
-    case "4"
-    console.log("Promedio:",promedio(aprendices));
-    break;
+            case "3"
+                console.log(reprobado(aprendices));
+                break;
 
-    case "5"
-    console.log(ordenarNotas([...aprendices]));
-    break;
+            case "4"
+                console.log("Promedio:",promedio(aprendices));
+                break;
 
-    case "6"
-    console.log(nombreMayusculas(aprendices));
-    break;
+            case "5"
+                console.log(ordenarNotas([...aprendices]));
+                break;
 
-    case "7"
-    let nota = parseFloat(prompt("ingresa una nota:"))
-    console.log (clasificar(nota))
-    break;
+            case "6"
+                console.log(nombreMayusculas(aprendices));
+                break;
 
-    case "0"
-    console.log("saliendo...")
-    break;
+            case "7"
+                let nota = parseFloat(prompt("ingresa una nota:"))
+                console.log (clasificar(nota))
+                break;
 
-    default:
-        console.log("opciones invalidas");
+            case "0"
+                console.log("saliendo...")
+                break;
 
-
-
+            default:
+                console.log("opciones invalidas");
     
-
- 
-    
-}
+        }
+    }
