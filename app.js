87efbc4e00b3f07fbbf4a4164d646730b62a7eb1
Axel Cortes <juliancortes5972@gmail.com>
2026-04-13 09:19:40 -0500

    Se termina toda la parte logica

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-function mostrarAprendices (lista){
-    lista.forEach (a => {
-        console.log(a.nombre, a.nota, a.programa)
-    });
-}
+function mostrarAprendices(lista) { 
+    lista.forEach(a => { 
+        console.log(a.nombre, a.nota, a.programa); 
+    }); 
+} 
+
+function aprobados(lista) { 
+    return lista.filter(a => a.nota >= 3.0); 
+} 
+
+function reprobados(lista) { 
+    return lista.filter(a => a.nota < 3.0); 
+} 
+
+function promedio(lista) { 
+    let total = lista.reduce((acc, a) => acc + a.nota, 0); 
+    return total / lista.length; 
+} 
+
