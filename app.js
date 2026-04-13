c982e834a8b04cee025480f898cb0103836c20d5
Axel Cortes <juliancortes5972@gmail.com>
2026-04-13 09:22:36 -0500

    se organiza el codigo

--- app.js
+++ app.js
@@ -65,21 +65,56 @@ function menu() {
 }
 
 function menu() {
- let opcion;
- while (opcion !== "0") {
- opcion = prompt("1.Ver todos\n2.Aprobados\n3.Salir");
- switch (opcion) {
- case "1":
- mostrarAprendices(aprendices);
- break;
- case "2":
- console.log(aprobados(aprendices));
- break;
- case "0":
- console.log("Saliendo...");
- break;
- default:
- console.log("Opcion invalida");
- }
- }
+    let opcion = "";
+
+    while (opcion !== "0") {
+        opcion = prompt(
+            "1. Ver todos\n" +
+            "2. Aprobados\n" +
+            "3. Reprobados\n" +
+            "4. Promedio\n" +
+            "5. Ordenar\n" +
+            "6. Mayúsculas\n" +
+            "7. Clasificar nota\n" +
+            "0. Salir"
+        );
+
+        switch (opcion) {
+            case "1":
+                mostrarAprendices(aprendices);
+                break;
+
+            case "2":
+                console.log(aprobados(aprendices));
+                break;
+
+            case "3":
+                console.log(reprobados(aprendices));
+                break;
+
+            case "4":
+                console.log("Promedio:", promedio(aprendices));
+                break;
+
+            case "5":
+                console.log(ordenarNotas([...aprendices]));
+                break;
+
+            case "6":
+                console.log(nombresMayus(aprendices));
+                break;
+
+            case "7":
+                let nota = parseFloat(prompt("Ingrese una nota:"));
+                console.log(clasificar(nota));
+                break;
+
+            case "0":
+                console.log("Saliendo...");
+                break;
+
+            default:
+                console.log("Opción inválida");
+        }
+    }
 }
